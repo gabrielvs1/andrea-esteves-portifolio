1c6e1b3255ee82fd58986f0f1e19184a80bb0d3e
Gabriel Esteves de Lima <brmou305186@ambev.com.br>
2024-12-04 20:57:08 -0300

    upload

---
+++ README.md
@@ -0,0 +1,46 @@
+<div align="center">
+<img style="width: 200px;" src="https://github.com/AlineEspindola/street-runner-portfolio/assets/117865319/0ed9c55d-f94f-49db-911c-bf0703a7afbd" />
+<h1>👟 Portfólio de um Corredor de Rua 🏃</h1>
+<h2><a href="https://sidney-espindola.vercel.app/">Acesse o Site</a></h2>
+</div>
+
+Um portfólio para captar patrocinadores para um corredor de rua. Foram criadas várias seções para convencer possíveis empresas a patrocinarem o atleta.
+
+https://github.com/AlineEspindola/street-runner-portfolio/assets/117865319/d70a4943-c9f6-4307-926c-f92ab8988cbc
+
+<hr>
+
+## Explorando as Camadas: Uma Jornada Visual e Informativa pela vida do Atleta
+
+A primeira seção é com um grande e exclusivo foco visual, demostrando, por meio da imagem, o intuito do site. A qualidade das imagens influencia demasidamente a opinião dos usuários.
+
+Há objetivo de exibir as maiores conquistas do atleta já no começo, na seção "PRINCIPAIS CONQUISTAS" para os usuários possuirem o conhecimento do nível do corredor. As imagens possuem a função de serem uma prova das vitórias, com os textos explicando os detalhes para maior compreensão.
+
+A seção "SOBRE" serve para contextualizar a vida do atleta, para ficar totalmente claro a história do corredor, para não gerar dúvida ao usuário e consequentemente receio, pois os indivíduos possuem medo do desconhecido.
+
+A seção "DADOS", com grandes números, é para captar a atenção por meio da grande quantidade de conquistas. Traz uma grande dimensão de admiração ao atleta.
+
+A seção "GALERIA" permite que o usuário veja dezenas de corridas participadas pelo atleta, afirmando a optidão do corredor, além de, novamente, captar atenção pela quantidade.
+
+A seção "FORMAS DE DIVULGAÇÃO" possui a finalidade de atrair ainda mais o interesse de possíveis patrocinadores, para dar a vontade de serem divulgados de tal modo. Esse desejo é mais ampliado após as seções anteriores, pois neste momento a ideia que o patrocinador já tenha sido "captado" (podendo o portfolio ser considerado uma landing pag = página de captura).
+
+Como prova social, a próxima seção "PATROCINADORES" é exibindo os patrocinadores que o atleta possui. Isso traz confiança ao possível patrocinador, uma vez que outras empresas resolveram apoiar o corredor.
+
+Neste momento, a ideia da seção "QUER PATROCINAR?" é que a empresa já esteja convecida a patrocinar, portanto, deve-se facilitar ao máximo o caminho para o patrocinio para não haver momento de desistência da empresa.
+
+<hr>
+
+<div align="center">
+<h3>꧁ 🔴 Autoria: Aline Espindola 🔴 ꧂</h3>
+
+[![Behance](https://img.shields.io/badge/-Behance-blue?style=for-the-badge&logo=behance&logoColor=white)](https://www.behance.net/line14)
+[![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/aline-espindola-72034b285)
+[![Gmail](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=alineabreuespindola@gmail.com)
+  
+</div>
+
+<hr>
+
+### Licença: GNU GENERAL PUBLIC LICENSE
+
+
---
+++ src/js/script.js
@@ -0,0 +1,144 @@
+function increaseNumber(element, interval, speed, max){
+    for (var i = 1; i <= max; i++) {
+        (function (j) {
+            setTimeout(function () {
+                element.innerHTML = '+'+j
+            }, j * (interval += speed))
+        })(i)
+    }
+}
+
+var occurredNumber1 = false
+var occurredNumber2 = false
+var occurredNumber3 = false
+
+const observer = new IntersectionObserver( (elements) => {
+    for(var i = 0; i < elements.length; i++){
+        if(elements[i].isIntersecting){
+            const input = elements[i].target || elements[i].srcElement
+
+            switch(input.id) {
+                case 'number1':
+                    if (!occurredNumber1) {
+                        increaseNumber(elements[i].target, 10, 10, 5)
+                        occurredNumber1 = true
+                    }
+                    break
+                case 'number2':
+                    if(!occurredNumber2) {
+                        increaseNumber(elements[i].target, 0.1, 0.19, 70)
+                        occurredNumber2 = true
+                    }
+                    break
+                case 'number3':
+                    if(!occurredNumber3) {
+                        increaseNumber(elements[i].target, 0.1, 0.1999, 200)
+                        occurredNumber3 = true
+                    }
+                    break
+                case 'main-races-title':
+                    animationSlideInUp(elements[i].target)
+                    break
+                case 'main-races-text':
+                    animationSlideInUp(elements[i].target)
+                    break
+                case 'main-races-card-1':
+                    animationFadeInUp(elements[i].target, 'animate__delay-1s')
+                    break
+                case 'main-races-card-2':
+                    animationFadeInUp(elements[i].target, 'animate__delay-2s')
+                    break
+                case 'main-races-card-3':
+                    animationFadeInUp(elements[i].target, 'animate__delay-3s')
+                    break
+                case 'about-img':
+                    animatioFadeInLeft(elements[i].target)
+                    break
+                case 'forms-disclosure-text':
+                    animatioFadeInLeft(elements[i].target)
+                    break
+                case 'sponsors-title':
+                    animationSlideInUp(elements[i].target)
+                    break
+                case 'sponsors-text':
+                    animationSlideInUp(elements[i].target)
+                    break
+                case 'sponsor-1':
+                    animationFadeIn(elements[i].target, 'animate__delay-1s')
+                    break
+                case 'sponsor-2':
+                    animationFadeIn(elements[i].target, 'animate__delay-2s')
+                    break
+                case 'sponsor-3':
+                    animationFadeIn(elements[i].target, 'animate__delay-3s')
+                    break
+                case 'sponsor-4':
+                    animationFadeIn(elements[i].target, 'animate__delay-4s')
+                    break
+                case 'sponsor-5':
+                    animationFadeIn(elements[i].target, 'animate__delay-5s')
+                    break
+                case 'sponsor-6':
+                    animationFadeIn(elements[i].target, 'animate__delay-6s')
+                    break
+                case 'sponsor-7':
+                    animationFadeIn(elements[i].target, 'animate__delay-7s')
+                    break
+                case 'sponsor-8':
+                    animationFadeIn(elements[i].target, 'animate__delay-8s')
+                    break
+            }
+        }
+    }
+})
+
+const animatedItems = document.querySelectorAll(".animated-item")
+
+animatedItems.forEach( (animatedItem) => observer.observe(animatedItem))
+
+window.onresize = function(){
+    getDimension()
+}
+
+function getDimension(){
+    return window.innerWidth
+}
+
+function checkSize(){
+    if(getDimension() > 991){
+        return 'grande'
+    }else{
+        return 'pequena'
+    }
+}
+
+function animationSlideInUp(element) {
+    element.classList.remove('animated-hidden-element')
+    element.classList.add('animate__animated', 'animate__slideInUp')
+}
+
+function animationFadeInUp(element, delay) {
+    if (checkSize() == 'pequena') {
+        element.classList.remove('animated-hidden-element')
+        element.classList.add('animate__animated', 'animate__fadeInUp')
+    } else {
+        element.classList.remove('animated-hidden-element')
+        element.classList.add('animate__animated', 'animate__fadeInUp', delay)
+    }
+}
+
+function animatioFadeInLeft(element) {
+    element.classList.remove('animated-hidden-element')
+    element.classList.add('animate__animated', 'animate__fadeInLeft')
+}
+
+function animationFadeIn(element, delay) {
+    if (checkSize() == 'pequena') {
+        element.classList.remove('animated-hidden-element')
+        element.classList.add('animate__animated', 'animate__fadeIn')
+    } else {
+        element.classList.remove('animated-hidden-element')
+        element.classList.add('animate__animated', 'animate__fadeIn', delay)
+    }
+}
+
